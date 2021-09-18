e4a659416314dca9ee1e20832fdcf0dd26c75184
mlopez <lopezmatthew87@gmail.com>
2021-09-18 16:35:32 -0400

    done with 22.3

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
 
-import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import ProductItem from "../ProductItem";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import spinner from "../../assets/spinner.gif";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -17,18 +18,34 @@ function ProductList() {
   useEffect(() => {
     if(data) {
       dispatch({
-           type: UPDATE_PRODUCTS,
-          products: data.products
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
         });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
